caccbc2284c567ee6af4b6f7677bb47c00108dd4
Kasper Horn Larsen <kasperhorn2003@gmail.com>
2023-01-13 13:42:11 +0100

    fix merge conflict and made ui for login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -23,74 +23,98 @@ class LoginPageState extends State<LoginPage> {
     passwordController.dispose();
   }
 
+  void _signin() async {
+    if (!formKey.currentState!.validate()) {
+      return;
+    }
+
+    final email = emailController.text;
+    final password = passwordController.text;
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      SnackBar snackBar = SnackBar(content: Text("${e.message}"));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } catch (e) {
+      SnackBar snackBar = SnackBar(content: Text("$e"));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login Page"),
-      ),
-      body: Form(
-        key: formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const Text("Sign in page", style: TextStyle(fontSize: 48)),
-              const Padding(padding: EdgeInsets.symmetric(vertical: 8)),
-              Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(hintText: "Enter email"),
-                    validator: ValidationBuilder().email().build(),
-                    controller: emailController,
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        colors: [Colors.red, Colors.yellow],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text("Sign in page"),
+          backgroundColor: const Color.fromARGB(100, 0, 0, 0),
+          shadowColor: Colors.transparent,
+        ),
+        body: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text("Sign in", style: TextStyle(fontSize: 48)),
+                const Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter email",
+                        filled: true,
+                        fillColor: Colors.grey,
+                      ),
+                      validator: ValidationBuilder().email().build(),
+                      controller: emailController,
+                    ),
+                    const Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter password",
+                        filled: true,
+                        fillColor: Colors.grey,
+                      ),
+                      validator: ValidationBuilder()
+                          .minLength(6)
+                          .maxLength(27)
+                          .build(),
+                      controller: passwordController,
+                    ),
+                  ],
+                ),
+                const Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                ElevatedButton(
+                  onPressed: (() => _signin()),
+                  child: const Text("Login"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    FocusManager.instance.primaryFocus?.unfocus();
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const SignupPage();
+                    }));
+                  },
+                  child: const Text(
+                    "Don't have an account?",
                   ),
-                  const Padding(padding: EdgeInsets.symmetric(vertical: 8)),
-                  TextFormField(
-                    decoration:
-                        const InputDecoration(hintText: "Enter password"),
-                    validator:
-                        ValidationBuilder().minLength(6).maxLength(27).build(),
-                    controller: passwordController,
-                  ),
-                ],
-              ),
-              const Padding(padding: EdgeInsets.symmetric(vertical: 8)),
-              ElevatedButton(
-                onPressed: () async {
-                  if (!formKey.currentState!.validate()) {
-                    return;
-                  }
-
-                  final email = emailController.text;
-                  final password = passwordController.text;
-
-                  try {
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: email,
-                      password: password,
-                    );
-                  } on FirebaseAuthException catch (e) {
-                    SnackBar snackBar = SnackBar(content: Text("${e.message}"));
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  } catch (e) {
-                    SnackBar snackBar = SnackBar(content: Text("$e"));
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  }
-                },
-                child: const Text("Login"),
-              ),
-              TextButton(
-                onPressed: () {
-                  FocusManager.instance.primaryFocus?.unfocus();
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) {
-                    return const SignupPage();
-                  }));
-                },
-                child: const Text("Don't have an account?"),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
