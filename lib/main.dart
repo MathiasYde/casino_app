1351422ea3ff16dadd4b13a1c952e73c592313d4
Mathias <mathias.yde@hotmail.dk>
2023-01-13 09:29:56 +0100

    Update main.dart to previous state

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,35 @@
+import 'package:casino_app/pages/error_page.dart';
+import 'package:casino_app/pages/home_page.dart';
+import 'package:casino_app/pages/login_page.dart';
+import 'package:casino_app/providers.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const ProviderScope(child: App()));
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _initialized = false;
-  bool _error = false;
+class App extends ConsumerWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
-  void initState() {
-    super.initState();
-    Firebase.initializeApp().then((_) {
-      setState(() {
-        _initialized = true;
-      });
-    }).catchError((error) {
-      setState(() {
-        _error = true;
-      });
-    });
-  }
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firebaseUser = ref.watch(firebaseUserProvider);
 
-  @override
-  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: _initialized
-              ? Text('Firebase connection established')
-              : _error
-                  ? Text('Error connecting to Firebase')
-                  : Text('Initializing...'),
-        ),
+      title: "Casino App",
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: firebaseUser.when(
+        data: (data) => (data == null) ? const LoginPage() : const HomePage(),
+        error: (error, stackTrace) => ErrorPage(error: error),
+        loading: () =>
+            const Scaffold(body: Center(child: CircularProgressIndicator())),
       ),
     );
   }
