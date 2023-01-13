4989a670d430b986032debdeac5c0101314eaf7c
Mathias <mathias.yde@hotmail.dk>
2023-01-13 09:36:07 +0100

    Made simple template in slotmachine_page.dart

---
+++ lib/pages/slotmachine_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SlotmachinePage extends StatelessWidget {
+  const SlotmachinePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Slotmachine Page"),
+      ),
+      body: const Center(
+        child: Text("SLOTMACHINE PAGE"),
+      ),
+    );
+  }
+}
