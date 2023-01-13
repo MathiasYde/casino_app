735cd651ba881a89863d410a09384783759239a2
Kasper Horn Larsen <kasperhorn2003@gmail.com>
2023-01-13 08:46:31 +0100

    firebase work, and connection tester app

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,43 @@
-import 'package:casino_app/pages/error_page.dart';
-import 'package:casino_app/pages/home_page.dart';
-import 'package:casino_app/pages/login_page.dart';
-import 'package:casino_app/providers.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(App());
+  runApp(MyApp());
 }
 
-class App extends ConsumerWidget {
-  const App({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _initialized = false;
+  bool _error = false;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final firebaseUser = ref.watch(firebaseUserProvider);
+  void initState() {
+    super.initState();
+    Firebase.initializeApp().then((_) {
+      setState(() {
+        _initialized = true;
+      });
+    }).catchError((error) {
+      setState(() {
+        _error = true;
+      });
+    });
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Casino App",
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blue,
-      ),
-      home: firebaseUser.when(
-        data: (user) {
-          return (user == null) ? const LoginPage() : const HomePage();
-        },
-        error: (error, stack) => ErrorPage(error: error),
-        loading: () => const Scaffold(
-          body: Center(
-            child: CircularProgressIndicator(),
-          ),
+      home: Scaffold(
+        body: Center(
+          child: _initialized
+              ? Text('Firebase connection established')
+              : _error
+                  ? Text('Error connecting to Firebase')
+                  : Text('Initializing...'),
         ),
       ),
     );
