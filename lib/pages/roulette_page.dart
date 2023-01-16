94a82702ba1f29071f60b76f247593476b9090ae
Mathias <mathias.yde@hotmail.dk>
2023-01-16 10:45:01 +0100

    Update roulette_page.dart

---
+++ lib/pages/roulette_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RoulettePage extends StatelessWidget {
+  const RoulettePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Roulette Page"),
+      ),
+      body: const Center(
+        child: Text("ROULETTE PAGE"),
+      ),
+    );
+  }
+}
