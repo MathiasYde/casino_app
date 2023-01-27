34f1a43e8bae0954a4e9fbd8de74f462d2db0d6e
Mathias <mathias.yde@hotmail.dk>
2023-01-27 11:05:44 +0100

    Update slotmachine_page.dart

--- lib/pages/slotmachine_page.dart
+++ lib/pages/slotmachine_page.dart
@@ -1,7 +1,65 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class SlotmachinePage extends StatelessWidget {
+class SlotmachinePage extends StatefulWidget {
   const SlotmachinePage({Key? key}) : super(key: key);
+
+  @override
+  State<SlotmachinePage> createState() => SlotmachinePageState();
+}
+
+class SlotmachinePageState extends State<SlotmachinePage>
+    with SingleTickerProviderStateMixin {
+  final int _slotsCount = 4;
+
+  List<String> iconFilepaths = [];
+
+  late List<Widget> items;
+
+  late List<int> _selectedItemIndicies;
+
+  late List<GlobalKey> _reelKeys;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedItemIndicies = List.generate(_slotsCount, (index) => 0);
+
+    items = List.generate(
+        iconFilepaths.length,
+        (index) => Image(
+              image: AssetImage(iconFilepaths[index]),
+            ));
+
+    _reelKeys = List.generate(
+        _slotsCount, (index) => GlobalKey<SlotMachineReelState>());
+  }
+
+  int getRandomTime(int min, int max) {
+    final random = Random();
+    return min + random.nextInt(max - min);
+  }
+
+  void spin() async {
+    for (GlobalKey reelKey in _reelKeys) {
+      SlotMachineReelState reelState =
+          reelKey.currentState as SlotMachineReelState;
+      reelState.startSpin();
+    }
+
+    await Future.delayed(Duration(milliseconds: getRandomTime(400, 1200)));
+
+    for (GlobalKey reelKey in _reelKeys) {
+      SlotMachineReelState reelState =
+          reelKey.currentState as SlotMachineReelState;
+
+      await Future.delayed(Duration(milliseconds: getRandomTime(600, 1700)));
+
+      reelState.stopSpin();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +67,93 @@ class SlotmachinePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Slotmachine Page"),
       ),
-      body: const Center(
-        child: Text("SLOTMACHINE PAGE"),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < _slotsCount; i++)
+                Expanded(
+                  child: Container(
+                      height: 200,
+                      child: SlotMachineReel(
+                        key: _reelKeys[i],
+                        reelItems: items,
+                        offAxisFraction: i / _slotsCount - 0.5,
+                      )),
+                ),
+            ],
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+          ElevatedButton(
+            onPressed: spin,
+            child: const Text("Spin"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SlotMachineReel extends StatefulWidget {
+  const SlotMachineReel({
+    Key? key,
+    required this.reelItems,
+    this.offAxisFraction = 0.0,
+  }) : super(key: key);
+
+  final List<Widget> reelItems;
+  final double offAxisFraction;
+
+  @override
+  State<SlotMachineReel> createState() => SlotMachineReelState();
+}
+
+class SlotMachineReelState extends State<SlotMachineReel>
+    with SingleTickerProviderStateMixin {
+  late FixedExtentScrollController _scrollController;
+
+  late Timer spinTimer;
+  late int counter;
+  final Duration continousSpinDuration = const Duration(milliseconds: 50);
+
+  void startSpin() {
+    counter = 0;
+    spinTimer = Timer.periodic(continousSpinDuration, (timer) {
+      _scrollController.animateToItem(
+        counter,
+        duration: continousSpinDuration,
+        curve: Curves.linear,
+      );
+      counter--;
+    });
+  }
+
+  void stopSpin() {
+    spinTimer.cancel();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = FixedExtentScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListWheelScrollView.useDelegate(
+      offAxisFraction: widget.offAxisFraction,
+      controller: _scrollController,
+      itemExtent: 100,
+      physics: const FixedExtentScrollPhysics(),
+      childDelegate: ListWheelChildLoopingListDelegate(
+        children: widget.reelItems,
       ),
     );
   }
