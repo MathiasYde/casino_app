14eef71784a7e9ae26b12ca1278f29adf13db294
Mathias <mathias.yde@hotmail.dk>
2023-01-31 12:07:15 +0100

    Force portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:casino_app/pages/login_page.dart';
 import 'package:casino_app/providers.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(const ProviderScope(child: App()));
 }
 
